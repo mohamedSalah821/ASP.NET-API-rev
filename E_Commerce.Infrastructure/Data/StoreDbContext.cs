881266578e3d28f91abd338a23b0c05355dd0804
mohamed salah <mohamedsalahsabry88@gmail.com>
2026-07-25 21:49:45 +0300

    Day01

---
+++ E_Commerce.Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Infrastructure.Data
+{
+    internal class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+    }
+
+}
---
+++ E_Commerce.Infrastructure/InfrastructureServicesRegisteration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Commerce.Infrastructure
+{
+    public static class InfrastructureServicesRegisteration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+
+    }
+}
+ 
