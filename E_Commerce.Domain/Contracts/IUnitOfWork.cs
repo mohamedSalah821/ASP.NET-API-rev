4a1f172432e62715eb4f38dd1ec11b83336e3025
mohamed salah <mohamedsalahsabry88@gmail.com>
2026-07-27 16:53:35 +0300

    Generic Repository and UnitOfWork

---
+++ E_Commerce.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.Domain.Common;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>;
+
+        Task<int> SaveChangesAsync(CancellationToken ct);
+    }
+}
